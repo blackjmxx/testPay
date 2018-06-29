90f5b952cd6a6e7879b3d6b8c340ecfd6c609c27
blacjmxx2 <alandou.jeanmichel@gmail.com>
2018-06-29 15:47:31 +0200

    check

--- public/payment.js
+++ public/payment.js
@@ -1,13 +1,14 @@
 function init() {
 
-    var trans_id = document.getElementById('trans_id');
+    var trans_id = '';
+    /*var trans_id = document.getElementById('trans_id');
     trans_id.value = Math.floor((Math.random() * 10000000) + 10000);
 
     var amount = document.getElementById('amount');
     amount.value = Math.floor((Math.random() * 100) + 5);
 
     var custom = document.getElementById('cpm_custom');
-    custom.value = Math.floor((Math.random() * 99999) + 7);
+    custom.value = Math.floor((Math.random() * 99999) + 7);*/
 
     //-------------Configuration
     CinetPay.setConfig({
@@ -18,9 +19,12 @@ function init() {
     //-------------Gestion des evenements
     //error
     CinetPay.on('error', function (e) {
+        $('.home-invite-friend-banner').attr("style", "display: block !important");
+        $('#az-header-wrapper').attr("style", "display: block !important");
         var error_div = document.getElementById('error_info');
-        error_div.innerHTML = '';
-        error_div.innerHTML += '<b>Error code:</b>' + e.code + '<br><b>Message:</b>:' + e.message;
+        error_div.innerHTML = "<div class='error form-group'><div class='alert-danger alert'>"+
+            "Opération interronpu, veuilez réessayer.</div></div>";
+        // error_div.innerHTML += '<b>Error code:</b>' + e.code + '<br><b>Message:</b>:' + e.message;
     });
     //ajax
     CinetPay.on('ajaxStart', function () {
@@ -34,21 +38,51 @@ function init() {
         console.log('Tocken généré: ' + token);
     });
     CinetPay.on('paymentPending', function (e) {
-        var error_div = document.getElementById('error_info');
-        error_div.innerHTML = 'Paiement en cours <br>';
-        error_div.innerHTML += '<b>code:</b>' + e.code + '<br><b>Message:</b>:' + e.message;
+        //$('.az-header-wrapper').css("display", "block");
+        //$('.home-invite-friend-banner').css("display", "block");
+        // var error_div = document.getElementById('error_info');
+        // error_div.innerHTML = 'Paiement en cours <br>';
+        //error_div.innerHTML += '<b>code:</b>' + e.code + '<br><b>Message:</b>:' + e.message;
     });
     CinetPay.on('paymentSuccessfull', function (paymentInfo) {
-        var error_div = document.getElementById('error_info');
-        var sucess_div = document.getElementById('success_info');
+
         if (typeof paymentInfo.lastTime != 'undefined') {
             if (paymentInfo.cpm_result == '00') {
-                error_div.innerHTML = '';
-                sucess_div.innerHTML = 'Votre paiement a été validé avec succès : <br> Montant :' + paymentInfo.cpm_amount + '<br>';
-                trans_id.value = Math.floor((Math.random() * 10000000) + 10000);
+                $('.home-invite-friend-banner').attr("style", "display: block !important");
+                $('#az-header-wrapper').attr("style", "display: block !important");
+                $.blockUI({ message: '<h3>Initialisation</h3><p>Veuillez patientez pendant l\'initialisation du transfert d\'argent</p>' });
+
+                $.ajax({
+                    type:"GET",
+                    //dataType: "json",
+                    url: "returns",
+                    data: {
+                        transaction_id:trans_id
+                        //phone:paymentInfo.cpm_phone_prefixe+paymentInfo.cel_phone_num
+                    }
+                })
+                    .done(function(data) {
+
+                        //swal.close();
+                        $.unblockUI();
+                        if(data.status){
+                            document.location.href = '/return/transfert/success';
+                        }else{
+                            document.location.href = '/return/transfert/error';
+                        }
+
+                    })
+                    .fail(function(jqXHR, textStatus) {
+                        $.unblockUI();
+                        console.log(jqXHR);
+                    });
+
             } else {
-                error_div.innerHTML = 'Une erreur est survenue :' + paymentInfo.cpm_error_message;
-                sucess_div.innerHTML = '';
+                $('.home-invite-friend-banner').attr("style", "display: block !important");
+                $('#az-header-wrapper').attr("style", "display: block !important");
+                var error_div = document.getElementById('error_info');
+                error_div.innerHTML = "<div class='error form-group'><div class='alert-danger alert'>"+
+                    "Opération a subi interruption, veuillez réessayer.</div></div>";
             }
         }
     });
@@ -56,13 +90,46 @@ function init() {
     //Application des méthodes
     var bt_get_signature = document.getElementById('bt_get_signature');
     bt_get_signature.addEventListener('click', function () {
-        CinetPay.setSignatureData({
+        /*CinetPay.setSignatureData({
             amount: parseInt(document.getElementById('amount').value),
             trans_id: document.getElementById('trans_id').value,
             currency: document.getElementById('currency').value,
             designation: document.getElementById('designation').value,
             custom: document.getElementById('cpm_custom').value
         });
-        CinetPay.getSignature();
+        CinetPay.getSignature();*/
+
+        //var amount = $('#amount').val();
+
+        $.ajax({
+            method:"GET",
+            url: "/recharge/news/"+$("#amount").val(),
+
+        })
+            .done(function(data) {
+                //document.getElementsByClassName('home-invite-friend-banner').style.display = 'none!important';
+                $('.home-invite-friend-banner').attr("style", "display: none !important");
+                $('#az-header-wrapper').attr("style", "display: none !important");
+                //$('#az-header-wrapper').css("display", "none!important");
+                //$('.home-invite-friend-banner').css("display", "none!important");
+
+                trans_id = data.transaction_id;
+                CinetPay.setSignatureData({
+                    amount: data.amount,
+                    trans_id: data.transaction_id,
+                    currency: data.currency,
+                    designation: data.designation,
+                    custom: data.cpm_custom
+                });
+                CinetPay.getSignature();
+
+            })
+            .fail(function(jqXHR, textStatus) {
+
+                console.log(jqXHR);
+            });
+
     });
+
 }
+
